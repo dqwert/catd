1569d295a8cf881ea41abc8e3a72a9bf5ccdc2dc
dqwerter <danielqin7@outlook.com>
2020-03-17 21:15:33 +0800

    (renamed)catd v0.2.0 merge

--- setup.py
+++ setup.py
@@ -4,15 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dqwert",
+    name="catd",
     version="0.1.0",
-    author="Wang Qin,
+    author="Wang Qin",
     author_email="danielqin7@outlook.com",
     description="A Chinese co-word analysis with topic discovery package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dqwerter/catd",
+    download_url='https://github.com/dqwerter/catd/archive/0.1.0.tar.gz',
     packages=setuptools.find_packages(),
+    install_requires=[  # I get to this in a second
+        'jieba',
+        'gensim',
+    ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
