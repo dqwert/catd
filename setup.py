583eeba26f3d46d85a8645891aa910b19852ca08
dqwerter <danielqin7@outlook.com>
2020-03-18 11:31:22 +0800

    cata v0.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dqwerter/catd",
-    download_url='https://github.com/dqwerter/catd/archive/0.1.0.tar.gz',
+    download_url='https://github.com/dqwerter/catd/archive/0.2.0.tar.gz',
     packages=setuptools.find_packages(),
     install_requires=[  # I get to this in a second
         'jieba',
